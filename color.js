7fca07454a10f109e8725d720e930b6209d04c0d
LybaaT <105508273+LybaaT@users.noreply.github.com>
2022-06-23 23:11:44 +0500

    creating a navigation for the color generator

--- color.js
+++ color.js
@@ -1,6 +1,6 @@
 const setBg = () => {
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
-    document.body.style.backgroundColor = "#" + randomColor;
+    document.getElementById('container1').style.backgroundColor = "#" + randomColor;
     color.innerHTML = "#" + randomColor;
     }
 
