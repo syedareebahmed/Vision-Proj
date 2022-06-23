f36dcb25e51c2a170523fc5994d63fead4a4500b
LybaaT <105508273+LybaaT@users.noreply.github.com>
2022-06-24 00:59:08 +0500

    color generator created

--- color.js
+++ color.js
@@ -1,8 +1,32 @@
-const setBg = () => {
+const setBg1 = () => {
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
-    document.getElementById('container1').style.backgroundColor = "#" + randomColor;
-    color.innerHTML = "#" + randomColor;
+    document.getElementById('color1').style.backgroundColor = "#" + randomColor;
+    color1.innerHTML = "#" + randomColor;
+    }
+    genNew.addEventListener("click", setBg1);
+    setBg1();
+
+    const setBg2 = () => {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.getElementById('color2').style.backgroundColor = "#" + randomColor;
+    color2.innerHTML = "#" + randomColor;
+    }
+    genNew.addEventListener("click", setBg2);
+    setBg2();
+
+    const setBg3 = () => {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.getElementById('color3').style.backgroundColor = "#" + randomColor;
+    color3.innerHTML = "#" + randomColor;
+    }
+    genNew.addEventListener("click", setBg3);
+    setBg3();
+
+    const setBg4 = () => {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.getElementById('color4').style.backgroundColor = "#" + randomColor;
+    color4.innerHTML = "#" + randomColor;
     }
 
-    genNew.addEventListener("click", setBg);
-    setBg();
+    genNew.addEventListener("click", setBg4);
+    setBg4();
