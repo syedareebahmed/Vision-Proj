d7706fba65d8fd5b61b4b41440a3c04ac8b85356
LybaaT <105508273+LybaaT@users.noreply.github.com>
2022-06-24 16:02:03 +0500

    added color bar in the color generator

--- color.js
+++ color.js
@@ -6,7 +6,7 @@ const setBg1 = () => {
     genNew.addEventListener("click", setBg1);
     setBg1();
 
-    const setBg2 = () => {
+const setBg2 = () => {
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     document.getElementById('color2').style.backgroundColor = "#" + randomColor;
     color2.innerHTML = "#" + randomColor;
@@ -14,7 +14,7 @@ const setBg1 = () => {
     genNew.addEventListener("click", setBg2);
     setBg2();
 
-    const setBg3 = () => {
+const setBg3 = () => {
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     document.getElementById('color3').style.backgroundColor = "#" + randomColor;
     color3.innerHTML = "#" + randomColor;
@@ -22,7 +22,7 @@ const setBg1 = () => {
     genNew.addEventListener("click", setBg3);
     setBg3();
 
-    const setBg4 = () => {
+const setBg4 = () => {
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     document.getElementById('color4').style.backgroundColor = "#" + randomColor;
     color4.innerHTML = "#" + randomColor;
@@ -30,3 +30,12 @@ const setBg1 = () => {
 
     genNew.addEventListener("click", setBg4);
     setBg4();
+
+const setBg5 = () => {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.getElementById('color5').style.backgroundColor = "#" + randomColor;
+    color5.innerHTML = "#" + randomColor;
+    }
+    
+    genNew.addEventListener("click", setBg5);
+    setBg5();
